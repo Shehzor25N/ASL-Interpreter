9440bd73a0f7047ab626b96c9b3f697c10e5aa50
Shehzor25N <shehzor381@gmail.com>
2024-09-29 02:24:54 +0400

    Added iOS

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'ionic.signionic',
-  appName: 'signionic',
+  appId: 'ionic.ASL',
+  appName: 'ASL Interpreter',
   webDir: 'www'
 };
 
