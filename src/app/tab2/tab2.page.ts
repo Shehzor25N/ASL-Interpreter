f6f9c24bba74e2365aaf52eeecb06c4518c14a95
Shehzor25N <shehzor381@gmail.com>
2024-09-29 21:23:33 +0400

    Added ML capabilities via Oracle/Flask

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,27 +1,64 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-import {
-  BleClient,
-  dataViewToText,
-  numbersToDataView,
-  ScanResult,
-} from '@capacitor-community/bluetooth-le';
-
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';  // Add these imports
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [HttpClientModule, IonicModule, CommonModule, FormsModule, ReactiveFormsModule]  // Import FormsModule and ReactiveFormsModule
 })
 export class Tab2Page {
-    bluetoothScanResults: ScanResult[] = [];
-  bluetoothIsScanning = false;
+  // Store the form values in this object
+  formValues = {
+    flex_1: null,
+    flex_2: null,
+    flex_3: null,
+    flex_4: null,
+    flex_5: null,
+    GYRx: null,
+    GYRy: null,
+    GYRz: null,
+    ACCx: null,
+    ACCy: null,
+    ACCz: null
+  };
 
-  bluetoothConnectedDevice?: ScanResult;
+  predictionResult: string | null = null;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
+  // Function to send data to Flask API
+  submitForm() {
+    const inputData = [
+      this.formValues.flex_1,
+      this.formValues.flex_2,
+      this.formValues.flex_3,
+      this.formValues.flex_4,
+      this.formValues.flex_5,
+      this.formValues.GYRx,
+      this.formValues.GYRy,
+      this.formValues.GYRz,
+      this.formValues.ACCx,
+      this.formValues.ACCy,
+      this.formValues.ACCz
+    ];
+
+    const apiUrl = 'http://100.101.248.5:5000/predict'; // Flask API URL
+
+    this.http.post(apiUrl, { data: inputData }).subscribe(
+      (response: any) => {
+        this.predictionResult = response.prediction;
+        console.log('Prediction Result:', this.predictionResult);
+      },
+      (error) => {
+        console.error('Error:', error);
+        this.predictionResult = 'No prediction available';
+      }
+    );
+  }
 }
