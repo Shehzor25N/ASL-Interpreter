9523d57e4b4b929f4feecc158d88206eeecbf189
Shehzor25N <shehzor381@gmail.com>
2024-09-29 21:32:31 +0400

    Refactor form inputs and submit functionality in tab2 page. Still needs work

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,64 +1,57 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';  // Import FormBuilder and FormGroup
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';  // Add these imports
+import { ReactiveFormsModule } from '@angular/forms';  // Use ReactiveFormsModule
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [HttpClientModule, IonicModule, CommonModule, FormsModule, ReactiveFormsModule]  // Import FormsModule and ReactiveFormsModule
+  imports: [HttpClientModule, IonicModule, CommonModule, ReactiveFormsModule]
 })
 export class Tab2Page {
-  // Store the form values in this object
-  formValues = {
-    flex_1: null,
-    flex_2: null,
-    flex_3: null,
-    flex_4: null,
-    flex_5: null,
-    GYRx: null,
-    GYRy: null,
-    GYRz: null,
-    ACCx: null,
-    ACCy: null,
-    ACCz: null
-  };
+  sensorForm: FormGroup;  // Declare the form
 
   predictionResult: string | null = null;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private formBuilder: FormBuilder) {
+    // Initialize the form with FormBuilder
+    this.sensorForm = this.formBuilder.group({
+      flex_1: [null, Validators.required],
+      flex_2: [null, Validators.required],
+      flex_3: [null, Validators.required],
+      flex_4: [null, Validators.required],
+      flex_5: [null, Validators.required],
+      GYRx: [null, Validators.required],
+      GYRy: [null, Validators.required],
+      GYRz: [null, Validators.required],
+      ACCx: [null, Validators.required],
+      ACCy: [null, Validators.required],
+      ACCz: [null, Validators.required]
+    });
+  }
 
-  // Function to send data to Flask API
+  // Function to submit form and make API call
   submitForm() {
-    const inputData = [
-      this.formValues.flex_1,
-      this.formValues.flex_2,
-      this.formValues.flex_3,
-      this.formValues.flex_4,
-      this.formValues.flex_5,
-      this.formValues.GYRx,
-      this.formValues.GYRy,
-      this.formValues.GYRz,
-      this.formValues.ACCx,
-      this.formValues.ACCy,
-      this.formValues.ACCz
-    ];
+    if (this.sensorForm.valid) {
+      const inputData = Object.values(this.sensorForm.value);
 
-    const apiUrl = 'http://100.101.248.5:5000/predict'; // Flask API URL
+      const apiUrl = 'http://100.101.248.5:5000/predict';  // Flask API endpoint
 
-    this.http.post(apiUrl, { data: inputData }).subscribe(
-      (response: any) => {
-        this.predictionResult = response.prediction;
-        console.log('Prediction Result:', this.predictionResult);
-      },
-      (error) => {
-        console.error('Error:', error);
-        this.predictionResult = 'No prediction available';
-      }
-    );
+      this.http.post(apiUrl, { data: inputData }).subscribe(
+        (response: any) => {
+          this.predictionResult = response.prediction;
+          console.log('Prediction Result:', this.predictionResult);
+        },
+        (error) => {
+          console.error('Error:', error);
+          this.predictionResult = 'No prediction available';
+        }
+      );
+    }
   }
 }
